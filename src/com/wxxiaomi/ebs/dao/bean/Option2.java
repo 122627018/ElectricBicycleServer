fb3e16ca404d7cc0ab36e8f8bb987fa198dd31cc
Administrator <122627018@qq.com>
2016-12-21 17:32:29 +0800

    d

--- src/com/wxxiaomi/ebs/dao/bean/Option2.java
+++ src/com/wxxiaomi/ebs/dao/bean/Option2.java
@@ -1,5 +1,16 @@
 package com.wxxiaomi.ebs.dao.bean;
 
-public class Option2 {
+import java.util.Date;
 
+public class Option2 {
+	private int id;
+	private int userid;
+	private String nickname;
+	private String avatar;
+	private String option_tag;
+	private Date create_time;
+	private String picture;
+	private String content;
+	private String foot;
+	
 }
